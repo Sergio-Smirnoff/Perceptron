bb66c7984ac71c306a17008c52d7241457c610f3
julianBogado1 <jbogado@itba.edu.ar>
2025-09-30 18:06:47 -0300

    non linear perceptron

---
+++ NonLinearPerceptron.py
@@ -0,0 +1,54 @@
+import json
+from LinearPerceptron import LinearPerceptron
+from SimplePerceptron import SimplePerceptron
+import numpy as np
+import random
+from tqdm import tqdm # Para la barra de progreso
+
+def activation_function(x:float) -> float:
+    with open("../params.json", "r") as f:
+        params = json.load(f)
+    beta = params.get("non_linear_beta", 1.0)
+    return 1 / (1 + np.exp(-2 * beta * x))
+
+class NonLinearPerceptron(LinearPerceptron):
+    def __init__(self, learning_rate:float, epochs:int=100, epsilon:float=0.01):
+        super().__init__(learning_rate, epochs, epsilon)
+        # self._step_activation_function = activation_function
+    def train(self, X: np.ndarray, z: np.array):
+        """Train linear perceptron with given training set and expected outputs.
+
+        Args:
+            X multidimensional array: Input variables.
+            z array: Expected outputs.
+        """
+        log_file = open("training_log.txt", "w")  
+        self.weights = [ random.uniform(-0.5,0.5) for _ in range(len(X[0])) ]
+        self.bias = random.uniform(-0.5, 0.5)
+        for epoch in tqdm(range(self.epochs), desc="Training..."):
+            sum_squared_error = 0.0
+
+            for i, x in enumerate(X):
+                linear_output = np.dot(x, self.weights) + self.bias
+                output = activation_function(linear_output)
+
+                error = z[i] - output
+                sum_squared_error += error**2
+
+                # update
+                for j in range(len(self.weights)):
+                    self.weights[j] += self.learning_rate * error * x[j]
+                self.bias += self.learning_rate * error
+
+                log_file.write(",".join(f"{w:.4f}" for w in self.weights) + f",{self.bias:.4f}\n")
+
+            mean_squared_error = sum_squared_error / len(X)
+            print(f'MSE at epoch {epoch+1}: {mean_squared_error}')
+
+            if mean_squared_error < self.epsilon:
+                print(f"Convergence reached at epoch {epoch+1}")
+                break
+
+        print(f"Training finished")
+        print(f"Bias={self.bias}")
+        log_file.close()
---
+++ ej2_linear.py
@@ -0,0 +1,33 @@
+from LinearPerceptron import LinearPerceptron
+import json
+import pandas as pd
+def parse_params(params):
+    with open(params) as f:
+        params = json.load(f)
+    learn_rate = params["learn_rate"]
+    epochs = params["epochs"]
+    epsilon = params["epsilon"]
+    input_file = params["input_file"]
+    output_file = params["output_file"]
+    return learn_rate, epochs, epsilon, input_file, output_file
+
+#====Parse CSV Training Data====
+def parse_training_data(file_path):
+    df = pd.read_csv(file_path)
+
+    # Each row is a variable -> support for multivalued functions
+    X = df.iloc[:, :-1].to_numpy(dtype=float)
+
+    #Last column is expected value
+    y = df['y'].to_numpy(dtype=float)
+    return X, y
+
+def main():
+    learn_rate, epochs, epsilon, input_file, output_file = parse_params("../params.json")
+    X, y = parse_training_data("../" + input_file)
+    perceptron = LinearPerceptron(learn_rate, epochs, epsilon)
+    perceptron.train(X, y)
+
+
+if __name__ == "__main__":
+    main()
