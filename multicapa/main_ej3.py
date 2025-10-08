fd767e5765bada419fc8501f8b457dc12c048eb6
Sergio Smirnoff <ssmirnoff@itba.edu.ar>
2025-10-08 01:07:41 -0300

    algunos analisis

--- multicapa/main_ej3.py
+++ multicapa/main_ej3.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import sys
 import traceback
@@ -6,10 +7,12 @@ from ej3_discriminacion_paridad import ParityMultyPerceptron
 
 # ==================== CONFIG ====================
 INPUT_PATH = "TP3-ej3-digitos.txt"
+INPUT_TEST_PATH = "input/TP3-ej3-digitos-test-light.txt"
 OUT_DIR = "outputs_ej3"
 DIGITS_OUTFILE = "digits_outputs.txt"
+OPTIMIZATION_MODE = "adam" # descgradient, adam, momentum
 
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.0001
 EPOCHS = 50000
 EPSILON = 1e-4
 LAYER_ONE_SIZE = 10
@@ -22,6 +25,14 @@ def find_input_file():
         return INPUT_PATH
     else:
         print(f"'{INPUT_PATH}' no está en el directorio actual.")
+        sys.exit(1)
+
+def find_test_file():
+    """Busca el archivo de entrada de test."""
+    if os.path.exists(INPUT_TEST_PATH):
+        return INPUT_TEST_PATH
+    else:
+        print(f"'{INPUT_TEST_PATH}' no está en el directorio actual.")
         sys.exit(1)
 
 
@@ -54,53 +65,70 @@ def load_digits_flat(path):
 
 def main():
     try:
-        input_file = find_input_file()
-        print("Usando archivo:", input_file)
+        if os.path.exists("outputs_ej3/" + DIGITS_OUTFILE):
+            os.system(f"rm -f outputs_ej3/{DIGITS_OUTFILE}")
+        predict_per_lable =[[],[],[],[],[],[],[],[],[],[]]
+        for j in range(10):
+            input_file = find_input_file()
+            print("Usando archivo:", input_file)
 
-        # === Cargar datos ===
-        X, y_digits = load_digits_flat(input_file)
-        # N, D = X.shape
-        for i in range(len(X)):
-            print(f'Dígito {i}: {X[i]}')
-
-        # Etiquetas de paridad: even -> 1, odd -> -1 (coincide con predict devuelto por la clase)
-        # y_parity = np.where((y_digits % 2) == 0, 1.0, -1.0).reshape(-1, 1)
-
-        expected_output = y_digits # digits
-
-        # Instanciar el perceptrón (usa la clase que pegaste arriba)
-        model = ParityMultyPerceptron(learning_rate=LEARNING_RATE, epochs=EPOCHS, epsilon=EPSILON, 
-                                      layer_one_size=5, layer_two_size=5, optimization_mode="adam") # descgradient, adam, momentum
-
-        # Entrenar
-        print("Iniciando entrenamiento...")
-        model.train(X, expected_output)
-        print("Entrenamiento finalizado.")
-
-        # Predecir: para cada muestra usamos model.forward_pass para obtener la salida cruda
-        preds_label = []
-        for i in range(len(X)):
-            xi = X[i] #xi es la matriz de bits de cada digito
-            
-            try:
-                pred_label = model.predict(xi) #TODO estamos prediciendo con los mismos valores que usamos para entrenar
-            except Exception:
-                pred_label = None
-            preds_label.append(pred_label)
-
-        # === Guardar resultados ===
-        os.makedirs(OUT_DIR, exist_ok=True)
-        out_path = os.path.join(OUT_DIR, DIGITS_OUTFILE)
-        with open(out_path, "w") as f:
-            f.write("digit\texpected_parity\n")
+            # === Cargar datos ===
+            X, y_digits = load_digits_flat(input_file)
+            # N, D = X.shape
             for i in range(len(X)):
-                expected = int(expected_output[i])
-                pred_l = preds_label[i]
-                f.write(f"{expected}\t{pred_l}\n")
-            print(f"expected: {expected_output}")
-            print("Predicciones (label):", preds_label)
+                print(f'Dígito {i}: {X[i]}')
 
-        print(f"Resultados guardados en: {out_path}")
+            # Etiquetas de paridad: even -> 1, odd -> -1 (coincide con predict devuelto por la clase)
+            # y_parity = np.where((y_digits % 2) == 0, 1.0, -1.0).reshape(-1, 1)
+
+            expected_output = y_digits # digits
+
+            # Instanciar el perceptrón (usa la clase que pegaste arriba)
+            model = ParityMultyPerceptron(learning_rate=LEARNING_RATE, epochs=EPOCHS, epsilon=EPSILON, 
+                                        layer_one_size=5, layer_two_size=5, optimization_mode=OPTIMIZATION_MODE) 
+            # Entrenar
+            print(f"Iniciando entrenamiento {j+1}...")
+            model.train(X, expected_output)
+            print(f"Entrenamiento finalizado {j+1}.")
+
+            # Predecir: para cada muestra usamos model.forward_pass para obtener la salida cruda
+            print(f"Realizando predicciones , iteracion {j+1}...")
+            test_file = find_test_file()
+            print("Usando archivo de test:", test_file)
+            X, y_digits = load_digits_flat(test_file)
+            preds_label = []
+            for i in range(len(X)):
+                xi = X[i]
+                try:
+                    pred_label = model.predict(xi)
+                except Exception:
+                    pred_label = None
+                predict_per_lable[i].append(pred_label)
+                preds_label.append(pred_label)
+
+            # === Guardar resultados ===
+            os.makedirs(OUT_DIR, exist_ok=True)
+            out_path = os.path.join(OUT_DIR, DIGITS_OUTFILE)
+            with open(out_path, "a") as f:
+                if j == 0:
+                    f.write(f"n;digit;expected_parity;{OPTIMIZATION_MODE}\n")
+                for i in range(len(X)):
+                    expected = int(expected_output[i])
+                    pred_l = preds_label[i]
+                    f.write(f"{j};{expected};{pred_l}\n")
+                print(f"expected: {expected_output}")
+                print("Predicciones (label):", preds_label)
+
+                print(f"Resultados guardados en: {out_path}")
+        with open(f"outputs_ej3/predictions.txt", "a") as f:
+            for i in range(len(predict_per_lable)):
+                counter = 0
+                for j in range(len(predict_per_lable[i])):
+                    if predict_per_lable[i][j] == i:
+                        counter += 1
+                f.write(f"{datetime.datetime.now()};{OPTIMIZATION_MODE};{i};{counter/10}\n")
+
+            
 
     except Exception:
         print("Ocurrió un error en main:")
