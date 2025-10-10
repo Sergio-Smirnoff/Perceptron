corrected scaling in ej2

NonLinearPerceptron.py
@@ -1,15 +1,20 @@
 # NonLinearPerceptron.py
 import numpy as np
 from tqdm import tqdm
+from sklearn.preprocessing import MinMaxScaler
 
 
 ## Funciones auxiliares
 def scale_array(array):
+    """Escala array de 1D!!!!!!!!
+    Output en (-1, 1)
+    """
     array = np.asarray(array, dtype=float)
-    array_min = np.min(array, axis=0)
-    array_max = np.max(array, axis=0)
+    array_min = np.min(array)
+    array_max = np.max(array)
     denom = (array_max - array_min)
-    denom[denom == 0] = 1e-9
+    # denom[denom == 0] = 1e-9
+    #Xnew= (Xold - Xmin)/(Xmax - Xmin) * (newMax - newMin) + newMin
     scaled = 2 * (array - array_min) / denom - 1
     return scaled, array_min, array_max
 
@@ -35,15 +40,26 @@ class NonLinearPerceptron:
         self.beta = beta
         self.weights = None
         self.bias = None
-        self.X_min = None
-        self.X_max = None
+        self.X_min = [[0.0],[0.0],[0.0]]
+        self.X_max = [[0.0],[0.0],[0.0]]
         self.y_min = None
         self.y_max = None
         self.history = []
 
     def train(self, X, y, X_test=None, y_test=None, train_log_file=None, test_log_file=None, verbose=False):
-        # Escalar datos de entrenamiento y guardar escaladores
-        X_scaled, self.X_min, self.X_max = scale_array(X)
+                
+        scaled_columns = []
+        for i in range(X.shape[1]):   # X.shape[1] = cantidad de columnas
+            col = X[:, i].reshape(-1, 1)  # Extraer columna como matriz columna
+            scaled_col, col_min, col_max = scale_array(col)  # Escalar la columna individualmente
+            scaled_columns.append(scaled_col)
+            if self.X_min[i] is None or self.X_min[i] > col_min:
+                self.X_min[i] = col_min
+            if self.X_max[i] is None or self.X_max[i] < col_max:
+                self.X_max[i] = col_max
+
+        # Reconstruir el array concatenando las columnas
+        X_scaled = np.hstack(scaled_columns)
         y_scaled, self.y_min, self.y_max = scale_array(y.reshape(-1, 1))
         y_scaled = y_scaled.flatten()
 
@@ -77,8 +93,9 @@ class NonLinearPerceptron:
             self.history.append(mse_scaled)
 
             # Calcular MSE sobre datos originales sin escalar
-            y_pred_train = self.predict(X)
-            train_mse = np.mean((y - y_pred_train) ** 2)
+            for xi, yi in zip(X, y):
+                y_pred_train = self.predict(xi)
+                train_mse = np.mean((yi - y_pred_train) ** 2)
 
             # Escribir en el log de entrenamiento si se pasa
             if train_log_file:
@@ -106,19 +123,22 @@ class NonLinearPerceptron:
             print(f"Training finished. Bias final: {self.bias:.6f}")
 
     def predict(self, x):
-        x = np.asarray(x, dtype=float)
-        is_single_item = x.ndim == 1
+        """
+        Realiza una predicción para una entrada dada.
+        x: Array de 3 elementos -> x=[x1, x2, x3]
+        """
 
-        x_input = x.reshape(1, -1) if is_single_item else x
-
-        denom = (self.X_max - self.X_min)
-        denom[denom == 0] = 1e-9
-        x_scaled = 2 * (x_input - self.X_min) / denom - 1
+        #Xnew= (Xold - Xmin)/(Xmax - Xmin) * (newMax - newMin) + newMin
+        x1 = (2 * (x[0] - self.X_min[0]) / (self.X_max[0] - self.X_min[0])) - 1
+        x2 = (2 * (x[1] - self.X_min[1]) / (self.X_max[1] - self.X_min[1])) - 1
+        x3 = (2 * (x[2] - self.X_min[2]) / (self.X_max[2] - self.X_min[2])) - 1
+        x_scaled = np.array([x1, x2, x3])
 
         linear = np.dot(x_scaled, self.weights) + self.bias
         y_scaled = activation_function(linear, self.beta)
+
         y_pred = descale_y(y_scaled, self.y_min, self.y_max)
 
-        return y_pred[0] if is_single_item else y_pred
+        return y_pred
 
 

ej2_non_linear.py
@@ -44,9 +44,18 @@ def test_perceptron(learn_rate, epochs, epsilon, X, y, beta=1.0, k_folds=10, see
         perceptron.train(X_train, y_train, verbose=False)
 
         # Predicción
-        preds = perceptron.predict(X_test)
-        mse = np.mean((preds - y_test) ** 2)
-        test_errors.append(mse)
+        preds=[]
+        mse=0.0
+        for xi, yi in zip(X_train, y_train):
+            y_pred_train = perceptron.predict(xi)
+            train_mse = np.mean((yi - y_pred_train) ** 2)
+
+            #predict([x1, x2, x3])
+
+            print(f"Fold {i+1}/{k_folds} - MSE (train set): {train_mse:.6f}")
+        # preds = perceptron.predict(X_test)
+        # mse = np.mean((preds - y_test) ** 2)
+            test_errors.append(train_mse)
 
         # Mostrar algunos ejemplos
         print(f"\n=== Fold {i+1}/{k_folds} ===")